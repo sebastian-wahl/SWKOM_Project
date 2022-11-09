722c74d1b0bb8500b7a648a53f95bd29a59dde9f
wchhay <33175698+wchhay@users.noreply.github.com>
2022-11-09 13:02:38 +0100

    Rename package

--- src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
+++ src/main/java/at/fhtw/swen3/persistence/entity/ParcelEntity.java
@@ -1,6 +1,6 @@
 package at.fhtw.swen3.persistence.entity;
 
-import at.fhtw.swen3.persistence.validation.annotation.MinExclusiveValidation;
+import at.fhtw.swen3.services.validation.annotation.MinExclusiveValidation;
 import lombok.*;
 
 import javax.persistence.*;
