ed5830b3a9fc9f0728f4f2988fb44c63696b6a0e
Willy Chhay <willymonivong27@gmail.com>
2022-11-15 15:19:07 +0100

    Send dummy data in ParcelServiceImpl

--- src/main/java/at/fhtw/swen3/services/impl/ParcelServiceImpl.java
+++ src/main/java/at/fhtw/swen3/services/impl/ParcelServiceImpl.java
@@ -2,11 +2,14 @@ package at.fhtw.swen3.services.impl;
 
 import at.fhtw.swen3.persistence.entities.ParcelEntity;
 import at.fhtw.swen3.services.ParcelService;
+import at.fhtw.swen3.services.dto.HopArrival;
 import at.fhtw.swen3.services.dto.NewParcelInfo;
 import at.fhtw.swen3.services.dto.TrackingInformation;
 import at.fhtw.swen3.services.validation.EntityValidatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
 
 
 @Service
@@ -28,16 +31,39 @@ public class ParcelServiceImpl implements ParcelService {
     @Override
     public NewParcelInfo submitParcel(ParcelEntity parcel) {
         entityValidatorService.validate(parcel);
-        return null;
+        return dummyNewParcelInfo();
     }
 
     @Override
     public TrackingInformation trackParcel(String trackingId) {
-        return null;
+        return dummyTrackingInformation();
     }
 
     @Override
     public NewParcelInfo transitionParcel(ParcelEntity parcel) {
-        return null;
+        entityValidatorService.validate(parcel);
+        return dummyNewParcelInfo();
+    }
+
+    private NewParcelInfo dummyNewParcelInfo() {
+        return new NewParcelInfo()
+                .trackingId("ABCDE6789");
+    }
+
+    private TrackingInformation dummyTrackingInformation() {
+        return new TrackingInformation()
+                .state(TrackingInformation.StateEnum.INTRANSPORT)
+                .addVisitedHopsItem(
+                    new HopArrival()
+                        .code("ABCD1")
+                        .description("visited")
+                        .dateTime(OffsetDateTime.now())
+                )
+                .addFutureHopsItem(
+                    new HopArrival()
+                        .code("ABCD2")
+                        .description("future")
+                        .dateTime(OffsetDateTime.now())
+                );
     }
 }
