23d1291adf58ed490de4c1aeac4fdfe7d7125c61
Willy Chhay <willymonivong27@gmail.com>
2022-11-15 13:18:24 +0100

    Refactor ConditionalValidator

--- src/main/java/at/fhtw/swen3/services/validation/annotation/ConditionalValidator.java
+++ src/main/java/at/fhtw/swen3/services/validation/annotation/ConditionalValidator.java
@@ -13,30 +13,33 @@ public class ConditionalValidator implements ConstraintValidator<ConditionalVali
     private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     private final Validator validator = factory.getValidator();
 
+    private String field;
+    private String[] contains;
+
+    @Override
+    public void initialize(ConditionalValidations constraintAnnotation) {
+        field = constraintAnnotation.field();
+        contains = constraintAnnotation.contains();
+    }
+
     @Override
     public boolean isValid(BaseEntity baseEntity, ConstraintValidatorContext context) {
         try {
-            ConditionalValidations annotation = baseEntity.getClass().getAnnotation(ConditionalValidations.class);
-            if (annotation != null) {
-                String field = annotation.field();
-                String[] matchingValues = annotation.contains();
+            Field privateField = baseEntity.getClass().getDeclaredField(field);
+            privateField.setAccessible(true);
+            String fieldValue = String.valueOf(privateField.get(baseEntity));
 
-                Field privateField = baseEntity.getClass().getDeclaredField(field);
-                privateField.setAccessible(true);
-                String fieldValue = String.valueOf(privateField.get(baseEntity));
+            if (Arrays.stream(contains).map(String::toLowerCase).toList().contains(fieldValue.toLowerCase())) {
+                Set<ConstraintViolation<BaseEntity>> violations = validator.validate(baseEntity, ValidateUnderCondition.class);
 
-                if (Arrays.stream(matchingValues).map(String::toLowerCase).toList().contains(fieldValue.toLowerCase())) {
-                    Set<ConstraintViolation<BaseEntity>> violations = validator.validate(baseEntity, ValidateUnderCondition.class);
-
-                    violations.forEach(violation -> context.buildConstraintViolationWithTemplate(violation.getMessage())
-                            .addPropertyNode(violation.getPropertyPath().toString())
-                            .addConstraintViolation());
-                    return violations.isEmpty();
-                }
+                violations.forEach(violation -> context.buildConstraintViolationWithTemplate(violation.getMessage())
+                        .addPropertyNode(violation.getPropertyPath().toString())
+                        .addConstraintViolation());
+                return violations.isEmpty();
             }
         } catch (NoSuchFieldException | IllegalAccessException e) {
             return false;
         }
-        return false;
+        return true;
     }
 }
