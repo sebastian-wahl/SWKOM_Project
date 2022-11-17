65b55fdd486b16fcc46b2efa868e759afd20eaa2
Sebastian Wahl <wahl.sebastian@outlook.com>
2022-11-17 09:57:03 +0100

    Fixed test

--- src/test/java/at/fhtw/swen3/services/validation/EntityValidatorServiceTest.java
+++ src/test/java/at/fhtw/swen3/services/validation/EntityValidatorServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.OffsetDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -39,6 +41,7 @@ class EntityValidatorServiceTest {
 
     private final HopArrivalEntity validHopArrivalEntity = HopArrivalEntity.builder()
             .parcel(ParcelEntity.builder().build())
+            .dateTime(OffsetDateTime.now())
             .code("FHBX799")
             .build();
 
