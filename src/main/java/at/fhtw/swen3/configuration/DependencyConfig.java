5de925026b193d07a69b4adf9639982c746c4fa4
sebastian-wahl <wahl.sebastian@outlook.com>
2022-11-28 14:02:18 +0100

    Added constructor dependency injection

---
+++ src/main/java/at/fhtw/swen3/configuration/DependencyConfig.java
@@ -0,0 +1,27 @@
+package at.fhtw.swen3.configuration;
+
+import at.fhtw.swen3.persistence.repositories.HopRepository;
+import at.fhtw.swen3.persistence.repositories.ParcelRepository;
+import at.fhtw.swen3.persistence.repositories.WarehouseRepository;
+import at.fhtw.swen3.services.ParcelService;
+import at.fhtw.swen3.services.WarehouseService;
+import at.fhtw.swen3.services.impl.ParcelServiceImpl;
+import at.fhtw.swen3.services.impl.WarehouseServiceImpl;
+import at.fhtw.swen3.services.validation.EntityValidatorService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DependencyConfig {
+
+    @Bean
+    public ParcelService parcelService(EntityValidatorService entityValidatorService, ParcelRepository parcelRepository, HopRepository hopRepository) {
+        return new ParcelServiceImpl(entityValidatorService, parcelRepository, hopRepository);
+    }
+
+    @Bean
+    public WarehouseService warehouseService(EntityValidatorService entityValidatorService, WarehouseRepository warehouseRepository, HopRepository hopRepository) {
+        return new WarehouseServiceImpl(entityValidatorService, warehouseRepository, hopRepository);
+    }
+
+}
