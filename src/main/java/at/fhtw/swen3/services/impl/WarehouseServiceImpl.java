9892d00fc6835443c0ba473cce4129be4c9825a2
Sebastian Wahl <wahl.sebastian@outlook.com>
2022-12-07 16:52:20 +0100

    Added logger to service classes, simple default debugging logs

--- src/main/java/at/fhtw/swen3/services/impl/WarehouseServiceImpl.java
+++ src/main/java/at/fhtw/swen3/services/impl/WarehouseServiceImpl.java
@@ -6,12 +6,14 @@ import at.fhtw.swen3.persistence.repositories.HopRepository;
 import at.fhtw.swen3.persistence.repositories.WarehouseRepository;
 import at.fhtw.swen3.services.WarehouseService;
 import at.fhtw.swen3.services.validation.EntityValidatorService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class WarehouseServiceImpl implements WarehouseService {
 
     @Autowired
@@ -25,18 +27,23 @@ public class WarehouseServiceImpl implements WarehouseService {
 
     @Override
     public WarehouseEntity exportWarehouses() {
+        log.debug("Exporting warehouses");
         // ToDo why only one warehouse (plural?)
         return new WarehouseEntity();
     }
 
     @Override
     public Optional<HopEntity> getWarehouse(String code) {
+        log.debug("Exporting warehouse with code {}", code);
         return hopRepository.findFirstByCode(code);
     }
 
     @Override
     public void importWarehouses(WarehouseEntity warehouse) {
+        log.debug("Importing warehouse");
         entityValidatorService.validate(warehouse);
+        log.debug("Given warehouse is valid");
         warehouseRepository.save(warehouse);
+        log.debug("Warehouse imported successfully");
     }
 }
