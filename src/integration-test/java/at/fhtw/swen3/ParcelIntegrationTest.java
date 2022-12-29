268a23b506f22f014df74f1a8baab9114033c61a
wchhay <33175698+wchhay@users.noreply.github.com>
2022-12-29 16:12:08 +0100

    Add parcel tracking call in ParcelIntegrationTest

--- src/integration-test/java/at/fhtw/swen3/ParcelIntegrationTest.java
+++ src/integration-test/java/at/fhtw/swen3/ParcelIntegrationTest.java
@@ -1,7 +1,12 @@
 package at.fhtw.swen3;
 
+import at.fhtw.swen3.services.dto.HopArrival;
+import at.fhtw.swen3.services.dto.NewParcelInfo;
+import at.fhtw.swen3.services.dto.TrackingInformation;
 import at.fhtw.swen3.util.HttpClient;
 import at.fhtw.swen3.util.impl.ParcelLogisticsServiceClient;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +29,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -52,37 +58,76 @@ class ParcelIntegrationTest {
 
     private HttpClient httpClient;
 
+    private ObjectMapper mapper;
+
     @BeforeEach
     void setUp() {
         String baseUrl = "http://localhost:" + port;
         httpClient = new ParcelLogisticsServiceClient(baseUrl, new TestRestTemplate());
+        mapper = new ObjectMapper();
     }
 
     @Test
     void test_whole_parcel_tracking_flow() throws IOException {
-        callImportWarehouses();
-        callSubmitParcel();
+        // 1. Import all warehouses from json file
+        postImportWarehouses();
+
+        // 2. Submit a parcel
+        String trackingId = postSubmitParcel();
+
+        // 3. Expect parcel tracking state to be PICKUP and futureHops have correct HopArrivals
+        getTrackParcel(trackingId);
+
     }
 
-    private void callImportWarehouses() throws IOException {
-        String body = readHopsFromFile("trucks-light-transferwh.json");
+    private void getTrackParcel(String trackingId) throws IOException {
+        // WHEN
+        ResponseEntity<TrackingInformation> response = httpClient.get("/parcel/" + trackingId, TrackingInformation.class);
 
+        // THEN
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+
+        assertThat(response.getBody().getState()).isEqualTo(TrackingInformation.StateEnum.PICKUP);
+        assertThat(response.getBody().getVisitedHops()).isEmpty();
+
+        List<HopArrival> expectedFutureHops = mapper.readValue(readFromFile("expected-future-hops.json"), new TypeReference<>(){});
+        assertThat(response.getBody().getFutureHops()).usingRecursiveFieldByFieldElementComparatorOnFields("code")
+                .containsSequence(expectedFutureHops);
+    }
+
+    private void postImportWarehouses() throws IOException {
+        // GIVEN
+        String body = readFromFile("trucks-light-transferwh.json");
+
+        // WHEN
         ResponseEntity<String> response = httpClient.post("/warehouse", body, String.class);
 
+        // THEN
         assertThat(response).isNotNull();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
-    private void callSubmitParcel() throws IOException {
-        String body = readHopsFromFile("submit-parcel.json");
-        ResponseEntity<String> response = httpClient.post("/parcel", body, String.class);
+    private String postSubmitParcel() throws IOException {
+        // GIVEN
+        String body = readFromFile("submit-parcel.json");
 
+        // WHEN
+        ResponseEntity<NewParcelInfo> response = httpClient.post("/parcel", body, NewParcelInfo.class);
+
+        // THEN
         assertThat(response).isNotNull();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(response.getBody()).contains("trackingId");
+
+        assertThat(response.getBody()).isNotNull();
+        String trackingId = response.getBody().getTrackingId();
+        assertThat(trackingId).isNotNull();
+
+        return trackingId;
     }
 
-    private String readHopsFromFile(String filename) throws IOException {
+    private String readFromFile(String filename) throws IOException {
         return Files.readString(Paths.get("src/integration-test/resources/" + filename));
     }
 }
